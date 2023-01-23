cf7b06bd3ef6cbb1c9ebe58f8de094df7886d527
Aanchal <bansalaanchal96@gmail.com>
2023-01-23 15:53:51 -0500

    Final commit

--- src/main/java/TestHarness.java
+++ src/main/java/TestHarness.java
@@ -2,8 +2,8 @@ public class TestHarness {
 
     public void GenerateSheep(){
 
-        for (int i = 1; i<11; i++) {
-            Sheep sheep1 = new Sheep("Sheep " + i, "Blue", 100, 40);
+        for (int i = 0; i<10; i++) {
+            Sheep sheep1 = new Sheep("Sheep " + i+1, "Blue", 100, 40);
             ListOfSheep los = new ListOfSheep();
             los.AddSheep(sheep1);
         }
